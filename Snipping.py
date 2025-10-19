2dca35c8b9ad79d70895fd0c80130ca0502e5d8f
DanielVargaAI <daniel.dv426@gmail.com>
2025-10-19 22:03:46 +0200

    Added Item names
    Added Item names of all items, as well as their sets.
    added items are in order

--- Snipping.py
+++ Snipping.py
@@ -1,22 +1,36 @@
 import pyautogui
 from PIL import Image
 from Data import *
+import os
 
 
-def snip_slots(left, top, right, bottom, picture):
+def snip_slots(left, top, right, bottom, lib_img):
     # Schneide den Bereich aus dem Bild aus
-    snipped_image = picture.crop((left, top, right, bottom))
+    snipped_image = lib_img.crop((left, top, right, bottom))
 
     return snipped_image
 
 
-if __name__ == "__main__":
-    # Picture ist LibraryTest.png
-    picture = Image.open("LibraryTest.png")
+def snip_library_slots():
+    """Snip all library slots from all library images."""
     counter = 0
-    for x in library[0]:
-        for y in library[1]:
-            snipped_image = snip_slots(x[0], y[0], x[1], y[1], picture)
-            save_name = str("snipped_images\\snipped_slot_" + str(counter) + ".png")
-            snipped_image.save(save_name)
-            counter += 1
+    # for all pages in the library images from folder Screenshots\Library
+    for lib_name in os.listdir("Screenshots\\Library"):
+        if lib_name.endswith(".png"):
+            lib_img = Image.open(os.path.join("Screenshots\\Library", lib_name))
+            for y in library[1]:
+                for x in library[0]:
+                    if lib_name.startswith("Page5"):
+                        # Apply offset for page 5
+                        snipped_slot = snip_slots(x[0], y[0]+26, x[1], y[1]+26, lib_img)
+                    else:
+                        snipped_slot = snip_slots(x[0], y[0], x[1], y[1], lib_img)
+                    save_name = str("snipped_images\\snipped_slot_" + str(counter) + ".png")
+                    snipped_slot.save(save_name)
+                    counter += 1
+
+
+if __name__ == "__main__":
+    snip_library_slots()
+
+# TODO: On Page 5 snip out only the new items (last 2 rows)
