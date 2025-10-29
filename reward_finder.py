286ae3338f120f54c86ad61028ddc81026f47dc6
Daniel <111242712+DanielVargaAI@users.noreply.github.com>
2025-10-29 13:15:16 +0100

    tested upscaling on reward reader
    implemented a small test for the reward reader, in hope to be able to read all numbers successfully.
    have to test on more data

--- reward_finder.py
+++ reward_finder.py
@@ -5,7 +5,7 @@ import cv2
 import easyocr
 
 # Load the image
-image_path = "Screenshots/InGame/InGame_61.png"
+image_path = "Screen.png"
 image = cv2.imread(image_path)
 
 # Define the ROI (x1, y1, x2, y2)
@@ -14,6 +14,9 @@ roi = (575, 30, 915, 70)
 # Crop the image to the ROI
 x1, y1, x2, y2 = roi
 cropped_image = image[y1:y2, x1:x2]
+
+# Scale up the cropped image
+cropped_image = cv2.resize(cropped_image, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
 
 # Initialize EasyOCR Reader
 reader = easyocr.Reader(['en'])  # Specify the language(s)
